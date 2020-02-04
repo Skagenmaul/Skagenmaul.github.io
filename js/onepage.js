d94b49fb38e58ececc2082440613e9341d4803bf
Skagenmaul <Skagenmaul@users.noreply.github.com>
2020-02-04 11:08:10 +0100

    js

---
+++ js/onepage.js
@@ -0,0 +1,13 @@
+$("#wrapper").css("overflow", "hidden");
+
+
+$("#firstDown").click(function(){
+
+  $('html, body').animate({
+        scrollTop: $("#box2").offset().top
+    }, 4000);
+
+
+
+});
+//test

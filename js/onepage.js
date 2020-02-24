d26f9c290e9cc588e561369cd44c0aa1b00a67b1
Skagenmaul <Skagenmaul@users.noreply.github.com>
2020-02-24 18:51:27 +0100

    fixed buttons w/ js (red = temp)

--- js/onepage.js
+++ js/onepage.js
@@ -2,13 +2,22 @@ $("#wrapper").css("overflow", "hidden");
 
 
 $("#firstDown").click(function(){
+console.log("hit");
 
   $('html, body').animate({
         scrollTop: $("#box2").offset().top
-    }, 4000);
+    }, 2000);
 
 
 
+});
+
+$("#box2").click(function(){
+console.log("hit");
+
+  $('html, body').animate({
+        scrollTop: $("#wrapper").offset().top
+    }, 2000);
 });
 //test
 //test2

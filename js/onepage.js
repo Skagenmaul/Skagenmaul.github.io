8a688118c73fec03d889810c0545e52fed9f9bb2
m.landgren <45962024+mlangren@users.noreply.github.com>
2020-03-16 10:07:16 +0100

    update

--- js/onepage.js
+++ js/onepage.js
@@ -1,1 +1,12 @@
 $("#wrapper").css("overflow", "hidden");
+$("#imgs").css("overflow", "show");
+
+var nummer = 0;
+
+$(document).ready(function () {
+  if(nummer==0){
+
+      $("#imgs").animate({bottom:"+204"},500,'linear');
+      nummer=nummer+1;
+  }
+}
